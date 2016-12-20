4040fae3cda73618f2b9276792a6f70fef4189cb
kyle uyehara <kuyehara1@gmail.com>
2016-12-19 14:27:32 -1000

    Solve Prob#5 -- Smallest Multiple --  of Project Euler

--- userTest.js
+++ userTest.js
@@ -1,7 +1,31 @@
+function smallestMultiple() {
+  const min = 1, max = 20;
+  let result = 1;
+
+  for (var i = min; i <= max; i++) {
+    splitIntoPrimeMultipliers(i, 2).forEach((number) => {
+      if (result % number === 0) result /= number;
+    });
+
+    result *= i;
+  }
+
+  return result;
+}
+
+function splitIntoPrimeMultipliers(num, from, result) {
+  result = result || [];
+  from = from > 1 ? from : 2;
+
+  for (let i = from; i <= num; i++) {
+    if (num % i === 0) {
+      result.push(i)
+      return splitIntoPrimeMultipliers(num / i, i, result);
+    }
+  }
+
+  return result;
+}
 console.time();
-var palindrome = 0;
-for(var i=100; i<=999; i++)
-  for(var j=100; j<=999; j++)
-    if((''+i*j).split('').reverse().join('')==(''+i*j))
-      palindrome = Math.max(palindrome,i*j);
+console.log(smallestMultiple());
 console.timeEnd();
